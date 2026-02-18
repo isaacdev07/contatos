3e268d702aedd29333ca1045923949e0a0467569
Isaac Santos <isaacsantos7953@gmail.com>
2026-02-17 21:47:58 -0300

    Refactor Main class for contact management

--- Celular/src/main/Main.java
+++ Celular/src/main/Main.java
@@ -10,7 +10,7 @@ import entidade.Mensagem;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method s
+		// TODO Auto-generated method 
 		
 		Scanner scn = new Scanner(System.in);
 
@@ -90,6 +90,7 @@ public class Main {
 	}
 
 }
+
 
 
 
